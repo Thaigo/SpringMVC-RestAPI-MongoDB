9899368c26102da2487d4947f69dc08a2fd32c2c
Thaigo_Freitas <tmfreitas@outlook.com>
2018-01-31 17:05:13 -0200

    create user entity controller and service

--- src/main/java/com/auth/entity/Usuario.java
+++ src/main/java/com/auth/entity/Usuario.java
@@ -2,6 +2,25 @@ package com.auth.entity;
 
 public class Usuario {
 	private String nome;
+	private String email;
+	private int idade;	
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
