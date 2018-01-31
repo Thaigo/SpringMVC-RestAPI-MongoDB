331049d2c025257734d8531869bfb3b041a9dd2b
Thaigo_Freitas <tmfreitas@outlook.com>
2018-01-31 16:58:08 -0200

    initial commit

---
+++ src/main/java/com/auth/AuthSpringIonicApplication.java
@@ -0,0 +1,12 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringIonicApplication.class, args);
+	}
+}
