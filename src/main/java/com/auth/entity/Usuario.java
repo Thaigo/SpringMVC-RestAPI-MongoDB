393e180483f0b8c531d33c85fdd25fb2a494b19d
Thaigo_Freitas <tmfreitas@outlook.com>
2018-01-31 18:04:24 -0200

    Add conection with mongodb and create usuariorepository

--- src/main/java/com/auth/entity/Usuario.java
+++ src/main/java/com/auth/entity/Usuario.java
@@ -1,10 +1,28 @@
 package com.auth.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Usuario {
+	
+	@Id
+	private String id;
+	
 	private String nome;
 	private String email;
 	private int idade;	
 	
+	
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getEmail() {
 		return email;
--- src/main/java/com/auth/service/UsuarioService.java
+++ src/main/java/com/auth/service/UsuarioService.java
@@ -1,44 +1,30 @@
 package com.auth.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.auth.entity.Usuario;
+import com.auth.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	public List<Usuario> listaUsuario() {
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> listaUsuario() {        
 
-        List<Usuario> usuarioLista = new ArrayList<>();
-
-        Usuario usuario = new Usuario();
-
-        usuario.setEmail("maria@gmail.com");
-        usuario.setIdade(26);
-        usuario.setNome("Maria");
-
-        usuarioLista.add(usuario);
-
-        usuario = new Usuario();
-
-        usuario.setEmail("fulano@gmail.com");
-        usuario.setIdade(35);
-        usuario.setNome("Fulano de tal");
-
-        usuarioLista.add(usuario);
-
-        return usuarioLista;
+        return usuarioRepository.findAll();
 
     }
 
     public List<Usuario> listaUsuario(Usuario usuarioAdd) {
 
-        List<Usuario> usuarioLista = this.listaUsuario();
-        usuarioLista.add(usuarioAdd);
+    	usuarioRepository.save(usuarioAdd);
 
-        return usuarioLista;
+        return usuarioRepository.findAll();
 
     }
 
